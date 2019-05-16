776dad0b6ba1d46fa1dc7000f36f2b47d20cd942
Nastassja Swierczynski <nastassja.swierczynski@gmail.com>
2019-05-16 15:24:59 +0200

    question app added

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @user_input = params[:question]
+    if @user_input == 'I am going to work'
+      @answer = 'Great'
+    elsif @user_input.include? '?'
+      @answer = 'get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
